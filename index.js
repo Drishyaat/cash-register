732ab12dc7ed22a3a52c5d63de2639f2d501f255
Drishyaat <drishyat2307@gmail.com>
2022-09-30 21:13:24 +0530

    updated index.js and style

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const checkButton= document.querySelector("#check-button");
 const message=document.querySelector("#error-message");
 const noOfNotes=document.querySelectorAll(".no-of-notes");
 //var numberOfNotes=0;
-const availableNotes=[2000 ,500 ,100 ,50 ,10 ,5 ,1 ];
+const availableNotes=[2000 ,500 ,100 ,20 ,10 ,5 ,1 ];
 
 checkButton.addEventListener("click", function validateBillandCash()
 {message.style.display="none"; 
